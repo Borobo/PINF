1a14d7fd0388f5c6cd1d3fce696ad7022d38f79e
Borobo <37670395+Borobo@users.noreply.github.com>
2019-02-24 20:41:16 +0100

    UPDATE(2.0) affichage_table.php
    +affichage des colonnes dans les bonnes tables
    +CSS des colonnes et des tables

--- affichage/affichage_table.php
+++ affichage/affichage_table.php
@@ -7,9 +7,12 @@ include("../header.html");
 
 		$(document).ready(function(){
 			//LES MODELES//////////////////////////////////////////
-			var modelJTable = $("<div class='tables col-sm-2 shadow bg-white text-center rounded border border-dark'>");
+			var modelJTable = $("<div class='tables shadow text-center rounded border border-dark'>");
 			var modelJLabel = $("<div class='title border border-right-0 border-left-0 border-top-0 border-dark'>");
+			var modelJData = $("<div class='container tab-data'>");
 			var modelJP = $("<p>");
+			var modelJColonne = $("<div class='card col shadow-sm bg-white'>");
+			var modelJLabCol = $("<div class='card-body text-left'>");
 			///////////////////////////////////////////////////////
 
 			$.getJSON("../data.php", {
@@ -18,14 +21,36 @@ include("../header.html");
 				function(oRep){
 					console.log(oRep);
 					for(var i=0; i<oRep.boards.length; i++){
+                        (function (i) {
 						var meta = oRep.boards[i];
 						//CREATION DU LABEL////////////////////////////////////////////////////
 						var unP = modelJP.clone().html("<b>"+meta.label+"</b>");
 						var unLabel = modelJLabel.clone(true).append(unP);
-						///////////////////////////////////////////////////////////////////////
-						var uneTable = modelJTable.clone(true).append(unLabel)
-							.data("label", meta.label); //On stocke le label dans le div.
+						///////////////////////////////////////////////////////////////////////text
+                        var lesData = modelJData.clone(true);
+                        var uneTable = modelJTable.clone(true).append(unLabel)
+                            .data("label", meta.label); //On stocke le label dans le div.
+
+                        $.getJSON("../data.php", {
+                                action : "getColonnes",
+                                idTable : meta.id},
+                            function(oCol){
+                                console.log(oCol);
+                                for(var j=0; j<oCol.colonnes.length; j++){
+                                    var meta2 = oCol.colonnes[j];
+                                    //CREATION DUNE COLONNE////////////////////////////////////////////////////
+                                    var unLabelCol = modelJLabCol.clone(true).html(meta2.label);
+                                    var uneColonne = modelJColonne.clone(true).append(unLabelCol);
+                                    ///////////////////////////////////////////////////////////////////////
+                                    lesData.append(uneColonne);
+                                }
+                            }
+
+                        );
+
+                        uneTable.append(lesData);
 						$("#content").append(uneTable);
+                        })(i);
 					}
 				}
 
@@ -39,18 +64,35 @@ include("../header.html");
 		#content{
 			background-color: lightblue;
 			display: flex;
-			overflow: auto; 
+			overflow: auto;
 		}
-		.tables{		
+		.tables{
+            position: relative;
 			height: 450px;
 			margin: 10px;
 			padding: 0;
-			min-width: 70px;
-		}
+            font-size: 9pt;
+            background-color: #dfe3e6 !important;
+            white-space: normal;
+            min-width: 272px;
+        }
 		.title{
+            font-size: 12pt;
 			width: 100%;
-			font-size: auto;
 		}
+        .tab-data{
+            position: absolute;
+            height: 85%;
+            overflow-y: auto;
+        }
+        .col{
+            display: flex;
+            margin: 10px 0;
+
+        }
+        .col div{
+            float: left;
+        }
 	</style>
 </header>
 
