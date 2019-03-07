d009ed92963316756aa38617fec3e91f4bdb2785
SachaLsr <37670395+Borobo@users.noreply.github.com>
2019-03-07 13:02:54 +0100

    Add files via upload

--- affichage/affichage_colonne.php
+++ affichage/affichage_colonne.php
@@ -4,6 +4,62 @@ include ("../header.html");
 
 <head>
     <script>
+    $(document).ready(function(){
+
+        //var modelJTable = $("<div class='tables shadow text-center rounded border border-dark'>");
+        var modelJLabel = $("<div>");
+        var modelJData = $("<div class='container-fluid container-colonnes'>");
+        var modelJP = $("<p>");
+        var modelJColonne = $("<div class='colonnes shadow text-center rounded border border-dark'>");
+        var modelJColonneCanvas = $("<div class='container border border-danger'>");
+        var modelJTable = $("<div class=\"tables\">");
+        var modelJLabCol = $("<div class='card-body text-left'>");
+
+        $.getJSON("../data.php", {
+            action : "getTables",
+            bdd : 1},
+            function(oRep){
+                console.log(oRep);
+                for(var i=0; i<oRep.boards.length; i++){
+                    (function (i) {
+                    var meta = oRep.boards[i];
+                    //CREATION DU LABEL////////////////////////////////////////////////////
+                    var unP = modelJP.clone();
+                    var unLabel = modelJLabel.clone(true);
+                    ///////////////////////////////////////////////////////////////////////text
+                    var lesData = modelJData.clone(true);
+                    var uneTable = modelJTable.clone(true).append(unLabel)
+                        .data("label", meta.label); //On stocke le label dans le div.
+
+                    $.getJSON("../data.php", {
+                            action : "getColonnes",
+                            idTable : meta.id
+                        },
+                        function(oCol){
+                            console.log(oCol);
+                            for(var j=0; j<oCol.colonnes.length; j++){
+                                (function(j){
+                                var meta2 = oCol.colonnes[j];
+                                //CREATION DUNE COLONNE/////////////////
+                                var unLabelCol = modelJLabCol.clone(true).html(meta2.label);
+                                var uneColonne = modelJColonne.clone(true).append(unLabelCol);
+                                ////////////////////////////////////////
+                                lesData.append(uneColonne);
+                            })(j)
+                            }
+                        }
+
+                    );
+
+                    uneTable.append(lesData).hide();
+                    $("#container-table").append(uneTable);
+                    })(i);
+                }
+            }
+
+        )
+
+  })
 
     </script>
 
@@ -15,7 +71,7 @@ include ("../header.html");
         overflow-y: hidden;
       }
       #container-fonction{
-        background-color: rgb(111, 176, 185);
+        background-color: rgb(100, 170, 255);
         height: 100%;
         width: 200px;
       }
@@ -30,15 +86,59 @@ include ("../header.html");
         margin-top: 50px;
         height: 100%;
       }
+      .tables:first-child{
+          display: block !important;
+      }
+      .tables
+      {
+          height: 100%;
+          width: 100%;
+          position: relative;
+          background-color: red;
+          
+      }
+      .colonnes{
+          position: relative;
+          height: 350px;
+          margin: 0;
+          font-size: 9pt;
+          background-color: #dfe3e6;
+          white-space: normal;
+          min-width: 200px;
+          display: flex;
+
+      }
+      .container-colonnes{
+          position: relative;
+          background-color: grey;
+          display: flex;
+          margin: auto;
+      }
+      .btn-light{
+        margin-bottom: 30px;
+        width: 180px;
+        height: 50px;
+        margin-left: 7px;
+      }
     </style>
 </head>
 
 
 <body>
   <div id="content">
-    <div id="container-fonction"></div>
+    <div id="container-fonction">
+              <button type="button" class="btn btn-light">Ajouter une ligne</button>
+              <button type="button" class="btn btn-light">Supprimer une ligne</button>
+              <button type="button" class="btn btn-light">Modifier une ligne</button>
+              <button type="button" class="btn btn-light">Gérer les doublons</button>
+              <button type="button" class="btn btn-light">Compter</button>
+              <button type="button" class="btn btn-light">Moyenne</button>
+              <button type="button" class="btn btn-light">Minimum</button>
+              <button type="button" class="btn btn-light">Maximum</button>
+    </div>
     <div id="affichage" class="container-fluide">
-      <div class="tables" id="container-table"></div>
+      <div class="tables" id="container-table">
+      </div>
     </div>
   </div>
 </body>
