4585d6b9a319a1409a124c3edba8415539f0cf1a
LucmanX76 <37670373+LucmanX76@users.noreply.github.com>
2019-04-01 16:32:55 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,78 @@
+<?php
+session_start()
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="../libs/bootstrap.css">
+
+    <script src="../libs/jquery-3.3.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+    <script src="../libs/bootstrap.min.js"></script>
+
+    <script type="text/javascript">
+        $(document).on("click", "input[type=submit]", function(contexte){
+            $.getJSON("../data.php",
+            {
+                action : "connexion",
+                identifiant : $("input[type=text]").val(),
+                password : $("input[type=password]").val()
+            }, function(oRep){
+                console.log("Connecté : "+oRep.connecte);
+                console.log(oRep.feedback);
+                if(!oRep.connecte){
+                    var alertBox = $("<div class='alert alert-danger'>")
+                        .html("<strong>Alerte</strong> : Identifiant ou mot de passe incorrect");
+                    $(".alert").remove();
+                    $("body").append(alertBox);
+                    setTimeout(function(){ alertBox.fadeOut("slow"); }, 5000);
+                } else {
+                    <?php
+                    if(isset($_SESSION["connecte"]) && $_SESSION["connecte"]){
+                        if($_SESSION["superadmin"]==0)
+                            header('Location:../affichage/affichageBDD.php');
+                        else
+                            header('Location:../affichage/affichageBDD_superadmin.php');
+                        }
+                    ?>
+                    window.location.reload();
+                }
+
+            })
+        });
+
+
+    </script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+    <h1>DBLOCK</h1>
+    <p>Formulaire de connexion</p>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+
+        </div>
+        <div class="col-sm-4 jumbotron text-center alert-dark">
+                <h1>Formulaire</h1><br>
+                    <input type="text" placeholder="Nom d'utilisateur" name="identifiant" required><br><br><!-- le required permet d'être obligé de rentrer une donnée-->
+
+                    <input type="password" placeholder="Mot de passe" name="password" required><br><br>
+
+                    <input type="submit" id='submit' value='Connexion'>
+
+        </div>
+        <div class="col-sm-4">
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
