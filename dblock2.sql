56f3a68fc2d892255c045d8b0f1988e0a3b6cb5d
SachaLsr <37670395+Borobo@users.noreply.github.com>
2019-05-02 16:16:48 +0200

    Add files via upload

--- dblock2.sql
+++ dblock2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 01 avr. 2019 à 14:02
--- Version du serveur :  5.7.19
--- Version de PHP :  5.6.31
+-- Généré le :  jeu. 02 mai 2019 à 14:16
+-- Version du serveur :  5.7.23
+-- Version de PHP :  7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -37,15 +37,19 @@ CREATE TABLE IF NOT EXISTS `bdd` (
   `creee` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `idCreateur` (`idCreateur`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `bdd`
 --
 
 INSERT INTO `bdd` (`id`, `nom`, `description`, `idCreateur`, `creee`) VALUES
-(1, 'bdd1', NULL, 1, 1),
-(2, 'bdd2', NULL, 1, 1);
+(1, 'bdd1', NULL, NULL, 1),
+(2, 'bdd2', NULL, NULL, 1),
+(3, 'bdd3', 'non créée', NULL, 0),
+(4, 'test', 'yo', 1, 1),
+(5, 'Magnolia', 'lol', 5, 1),
+(6, 'Magnolia 2 ', 'mol', 6, 1);
 
 -- --------------------------------------------------------
 
@@ -59,22 +63,23 @@ CREATE TABLE IF NOT EXISTS `colonne` (
   `label` varchar(40) DEFAULT NULL,
   `description` varchar(300) DEFAULT NULL,
   `idTab` int(11) DEFAULT NULL,
+  `type` varchar(20) NOT NULL,
+  `A_I` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `idTab` (`idTab`)
-) ENGINE=MyISAM AUTO_INCREMENT=57 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=113 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `colonne`
 --
 
-INSERT INTO `colonne` (`id`, `label`, `description`, `idTab`) VALUES
-(1, 'prenom', NULL, 1),
-(2, 'nom', NULL, 1),
-(3, 'id', NULL, 2),
-(4, 'marque', NULL, 2),
-(5, 'date', NULL, 2),
-(56, 'titre', '1', 75),
-(55, 'a', '1', 69);
+INSERT INTO `colonne` (`id`, `label`, `description`, `idTab`, `type`, `A_I`) VALUES
+(108, 'telephone', '', 103, 'Nombre', 0),
+(107, 'adresse', 'adresse de l\'utilisateur', 103, 'Texte', 0),
+(106, 'prenom', 'prenom de l\'utilisateur', 103, 'Texte', 0),
+(104, 'id', 'id de l\'utilisateur', 103, 'Nombre', 0),
+(105, 'nom', 'nom de l\'utilisateur', 103, 'Texte', 0),
+(109, 'a', 'a', 105, 'Texte', 0);
 
 -- --------------------------------------------------------
 
@@ -85,21 +90,33 @@ INSERT INTO `colonne` (`id`, `label`, `description`, `idTab`) VALUES
 DROP TABLE IF EXISTS `data`;
 CREATE TABLE IF NOT EXISTS `data` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `valInt` float DEFAULT NULL,
+  `valInt` decimal(10,0) DEFAULT NULL,
   `valChar` varchar(1000) DEFAULT NULL,
   `idColonne` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `idColonne` (`idColonne`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=78 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `data`
 --
 
 INSERT INTO `data` (`id`, `valInt`, `valChar`, `idColonne`) VALUES
-(1, NULL, 'sacha', 1),
-(2, NULL, '', 2),
-(3, 1, NULL, 3);
+(77, '623335740', NULL, 108),
+(76, NULL, NULL, 107),
+(69, NULL, 'TESSON ', 105),
+(75, NULL, 'test', 106),
+(74, NULL, 'TEST', 105),
+(70, NULL, 'Lucas', 106),
+(71, NULL, NULL, 107),
+(72, NULL, NULL, 108),
+(73, '3', NULL, 104),
+(67, NULL, NULL, 108),
+(68, '2', NULL, 104),
+(66, NULL, NULL, 107),
+(65, NULL, 'Sacha', 106),
+(64, NULL, 'LESUEUR', 105),
+(63, '1', NULL, 104);
 
 -- --------------------------------------------------------
 
@@ -137,8 +154,15 @@ CREATE TABLE IF NOT EXISTS `liste_user` (
 --
 
 INSERT INTO `liste_user` (`idBdd`, `idUser`, `admin`) VALUES
-(2, 1, 1),
-(1, 1, 1);
+(1, 1, 0),
+(2, 1, 0),
+(1, 2, 1),
+(2, 2, 1),
+(1, 6, 0),
+(1, 4, 1),
+(1, 5, 1),
+(5, 5, 1),
+(6, 6, 1);
 
 -- --------------------------------------------------------
 
@@ -155,18 +179,26 @@ CREATE TABLE IF NOT EXISTS `tab` (
   PRIMARY KEY (`id`),
   KEY `idBdd` (`idBdd`),
   KEY `idUser` (`idUser`)
-) ENGINE=MyISAM AUTO_INCREMENT=76 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=108 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `tab`
 --
 
 INSERT INTO `tab` (`id`, `label`, `idBdd`, `idUser`) VALUES
-(1, 'user', 1, NULL),
-(2, 'voitures', 1, NULL),
-(3, 'test', 1, NULL),
+(90, 'lol', NULL, NULL),
 (7, 'PAS BIEN', 2, NULL),
-(75, 'missions', 1, NULL);
+(89, 'lol', NULL, NULL),
+(88, 'test', NULL, NULL),
+(87, 'Users', NULL, NULL),
+(91, 'table', NULL, NULL),
+(92, 'test1', NULL, NULL),
+(93, 'l', NULL, NULL),
+(94, '1', NULL, NULL),
+(95, 'lo', NULL, NULL),
+(96, 'test123', NULL, NULL),
+(105, 'test', 1, 5),
+(103, 'Utilisateur', 1, 5);
 
 -- --------------------------------------------------------
 
